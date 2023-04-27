645265ad05de7cacff8537519cf869ff5edca1e1
Inna Dmytrenko <inna.shalun@gmail.com>
2023-04-27 21:47:56 +0300

    task 01

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,24 @@
-export const App = () => {
+import user from './Profile/user.json';
+import Profile from "./Profile/Profile";
+
+import '../index.css';
+
+export default function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
+      <div style={{
         display: 'flex',
-        justifyContent: 'center',
         alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+        justifyContent: 'center',
+        height: '100vh',}}>
+
+        <Profile
+          key={user.tag}
+          avatar={user.avatar}
+          username={user.username}
+          tag={user.tag}
+          location={user.location}
+          stats={user.stats}
+        />
+      </div>
   );
 };
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,43 @@
+import PropTypes from 'prop-types';
+import {CardUser, UserName, Avatar, Name, Info, Stats, StatsItem, StatsInformation, StatsInfoQuantity} from './Profile.styled'
+
+function Profile({avatar, username, tag, location, stats}) {
+    return (
+        <CardUser>
+            <Avatar src={avatar} alt='User avatar'/>
+            <UserName>
+                <Name>{username}</Name>
+                <Info>@{tag}</Info>
+                <Info>{location}</Info>
+            </UserName>
+            <Stats>
+                <StatsItem>
+                    <StatsInformation>Followers</StatsInformation>
+                    <StatsInfoQuantity>{stats.followers}</StatsInfoQuantity>
+                </StatsItem>
+                <StatsItem>
+                    <StatsInformation>Views</StatsInformation>
+                    <StatsInfoQuantity>{stats.views}</StatsInfoQuantity>
+                </StatsItem>
+                <StatsItem>
+                    <StatsInformation>Likes</StatsInformation>
+                    <StatsInfoQuantity>{stats.likes}</StatsInfoQuantity>
+                </StatsItem>
+            </Stats>
+        </CardUser>
+    )
+}
+
+
+Profile.propTypes = {
+    username: PropTypes.string.isRequired,
+    tag: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired, 
+    stats: PropTypes.exact({
+        followers: PropTypes.number.isRequired,
+        views: PropTypes.number.isRequired,
+        likes: PropTypes.number.isRequired,
+    }),
+};
+
+export default Profile;
